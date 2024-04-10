aff49558ba8cfd9c47e0762278fe8d327f7f0863
anuj8848 <anujgiri8848@gmail.com>
2024-04-10 10:48:52 +0545

    2 input samples addede, total 3 input samples/feature set instances/observations

--- neurons_without_class.py
+++ neurons_without_class.py
@@ -1,9 +1,10 @@
 import numpy as np
-inputs=np.array([1,2,3,4])
+inputs=np.array([[1,2,3,4],[3,4.5,-2.8,4.5],[3.9,1,9,-3.1]])
 bias=np.array([2,3,0.69])
 
 
 weights=np.array([[0.23,-0.21,-0.93,1.2],[0.2,-0.89,0.57,-0.43],[-0.23,-0.46,0.96,-0.11]])
 
-output=np.dot(weights,inputs)+bias
+output=np.dot(inputs,weights.T)+bias
 print(output)
+
